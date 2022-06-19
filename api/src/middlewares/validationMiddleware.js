b2e5ee44810686b983a24b1ebeb9681e8e4e39c1
tunnelview <86519065+tunnelview@users.noreply.github.com>
2022-06-19 10:59:00 +1000

    Add middileware Joi to node src

---
+++ api/src/middlewares/validationMiddleware.js
@@ -0,0 +1,22 @@
+import Joi from "joi";
+
+const adminRegistrationValication = (req, res, next) => {
+  console.log(req.body);
+  //first check if data is clean or not if it is not clear then response
+  // from here with the error msg otherwise let express to continue exe code to the next middleware by calling next ()
+
+  const schema = Joi.object({
+    Fname: Joi.string().min(3).max(50),
+    lName: Joi.string().min(3).max(50),
+    Dob: Joi.string().min(3).max(50),
+    email: Joi.string().min(3).max(50),
+    password: Joi.string().min(6).max(50),
+    location: Joi.string().allow(""),
+  });
+
+  res.json({
+    status: "error",
+    message: "TODO invalid data",
+  });
+  next();
+};
